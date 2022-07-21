51967fe9da95e950c6a3d90384f4560cb48c3349
Ashley Hong <ashley.hong@duke.edu>
2022-07-21 00:15:15 -0400

    Add body parsing middleware

--- server/index.js
+++ server/index.js
@@ -2,14 +2,16 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const port = process.env.PORT || 5000;
+var bodyParser = require('body-parser'); // Calling body-parser to handle req object from POST requests
 
 // This displays message that the server running and listening to specified port
 app.listen(port, () => console.log(`Listening on port ${port}`));
-
 // Have Node serve the files for our built React app
 app.use(express.static(path.resolve(__dirname, "../build")));
+// Parse application/json (Parse incoming req object as a JSON Object)
+app.use(bodyParser.json());
 
-// create a GET route
+// Create a GET route
 app.get("/api", (req, res) => {
 	res.json({ word: "test" });
 });
